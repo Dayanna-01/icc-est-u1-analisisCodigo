d02afedba1a51e3ecea6fc6e0c8fd4fb0c976fb6
Dayanna Chacha <jchachav@est.ups.edu.ec>
2025-05-01 08:38:30 -0500

    Trabajo Clases Acabada

--- bin/src_phyton/app.py
+++ bin/src_phyton/app.py
@@ -1,6 +1,28 @@
-#import benchmarking  as Ben #Nombre que le damos a la clase
-from benchmarking import Benchmarking #Otra forma de importar archivos
+# from benchmarking import Benchmarking  # si benchmarking.py está en el mismo directorio
+from benchmarking import Benchmarking
+from metodos_ordenamiento import MetodosOrdenamiento
 
-if __name__ == "__main__": #Constructor
+if __name__ == "__main__":
     print("Funciona")
-    Benchmarking()
+    # Instanciar clases
+    metodos = MetodosOrdenamiento()
+    benchmarking = Benchmarking()
+
+    tam = 1000
+    arreglo_base = benchmarking.build_arreglo(tam)
+
+
+    metodosD= {
+        "burbuja": metodos.sortByBubble,
+        "seleccion": metodos.sortBySeleccion
+    }
+
+    resultados = []
+    for nombre, metodo in metodosD.items():
+        tiempo=benchmarking.medir_tiempo(metodo, arreglo_base)
+        tuplaResultado=(tam,nombre,tiempo)
+        resultados.append(tuplaResultado)
+
+    for resultado in resultados:
+        tam,nombre,tiempo=resultado
+        print(f"tamano: {tam}, metodo: {nombre}, tiempo: {tiempo:.6f} segundos")
--- bin/src_phyton/metodos_ordenamiento.py
+++ bin/src_phyton/metodos_ordenamiento.py
@@ -1,10 +1,37 @@
 class MetodosOrdenamiento:
-# Metodo Burbuja en phyton
-    def sortByBubble(self, arreglo): #Self es la instancia de la clase y es obligatorio 
+    def sortByBubble(self, arreglo):
         arreglo = arreglo.copy()
-        n = len(arreglo) 
+        n = len(arreglo)
         for i in range(n):
-            for j in range(i+1, n):
-                if arreglo[i] > arreglo[j]:
-                    arreglo[i], arreglo[j] = arreglo[j], arreglo[i]
+            for j in range(0, n - i - 1):
+                if arreglo[j] > arreglo[j + 1]:
+                    arreglo[j], arreglo[j + 1] = arreglo[j + 1], arreglo[j]
         return arreglo
+
+    def sortBySeleccion(self, array):
+        array = array.copy()
+        n = len(array)
+        for i in range(n):
+            minindx = i
+            for j in range(i + 1, n):
+                if array[j] < array[minindx]:
+                    minindx = j
+            array[i], array[minindx] = array[minindx], array[i]
+        return array
+    
+    def sortByShell(self, arreglo):
+        arreglo = arreglo.copy()
+        n = len(arreglo)
+        gap = n // 2
+        while gap > 0:
+            for i in range(gap, n):
+                temp = arreglo[i]
+                j = i
+                while j >= gap and arreglo[j - gap] > temp:
+                    arreglo[j] = arreglo[j - gap]
+                    j -= gap
+                arreglo[j] = temp
+            gap //= 2
+        return arreglo
+    
+    
